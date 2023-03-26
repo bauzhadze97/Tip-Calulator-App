efa671a49d9906140abd1f33d372fc734daac5da
bauzhadze97 <bauzhadzedev@gmail.com>
2023-03-27 00:38:25 +0400

    Working on mobile

---
+++ script.js
@@ -0,0 +1,8 @@
+function getVal() {
+    const val = document.getElementById('bill').value;
+     console.log(val * 100);
+}
+function getTip() {
+    const tip = document.getElementById('tip').value;
+    console.log(tip);
+}
