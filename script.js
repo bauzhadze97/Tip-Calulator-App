a7641cd82c11fbd11a091d53ac8d7a4e74b96498
bauzhadze97 <bauzhadzedev@gmail.com>
2023-03-27 21:56:44 +0400

    Working on js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function getVal() {
-    const val = document.getElementById('bill').value;
-     console.log(val * 100);
-}
-function getTip() {
-    const tip = document.getElementsByName('tip').value;
-    console.log(tip);
-}
+const bill = document.getElementById('bill').value;
+const customTip = document.getElementById('custom').value;
+const people = document.getElementById('numberpeople').value;
+const button = document.querySelectorAll('.tip button').value;
+const tipvalue = document.getElementById('tipvalue').value;
+
+button.forEach((button) => {
+    button.addEventlistener("click", (e) => {
+        let tipvalue = e.target.innerText;
+        tipvalue = tipvalue.substr(0, tipvalue.lenght -1);
+        if (bill.value === "") return;
+        if (people.value === "") people.value = 1;
+        calculateTip(
+            parseFloat(bill.value),
+            parseInt(tipvalue),
+            parseInt(people.value)
+        );
+    });
+});
